9b2f451b0e4172852a005ed8c5cb1ae134955bb9
Jacob Daddario <jake.daddario@gmail.com>
2020-05-14 01:33:21 -0500

    Wrote controller actions for CRUD actions of subscriptions

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,7 +1,16 @@
 class SubscriptionsController < ApplicationController
 	def create
+		chatroom = Chatroom.find(params[:chatroom_id])
+
+		current_user.subscriptions.create(chatroom_id: chatroom.id)
+		flash[:info] = "Successfully subscribed to the #{chatroom.name}."
 	end
 
 	def destroy
+		subscription = Subscription.find(params[:id])
+		chatroom = Chatroom.find(params[:id])
+
+		subscription.destroy
+		flash[:info] = "Successfully unsubscribed from #{chatroom.name}."
 	end
 end
