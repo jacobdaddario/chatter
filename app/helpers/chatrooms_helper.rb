730b93fdf59ce888edf8b8377f2c1b312f3dd462
Jacob Daddario <jake.daddario@gmail.com>
2020-05-20 15:05:54 -0500

    Completed basic functionality for adding chatrooms to the page. Ran into a bug where subscribing to a chatroom and then trying to join it doesn't work. Need to investigagte

--- app/helpers/chatrooms_helper.rb
+++ app/helpers/chatrooms_helper.rb
@@ -1,6 +1,6 @@
 module ChatroomsHelper
 	def subscribe_or_show(user, chatroom)
-		user.subscribed?(chatroom) ? link_to(chatroom.name, "#", class: "flex-grow mr-6 font-medium text-lg text-gray-900 hover:underline", data: { behavior: "chatroom-name" })
+		user.subscribed?(chatroom) ? render("chatrooms/link", chatroom: chatroom)
 		 													 : render("subscriptions/form", user: user, chatroom: chatroom )
 	end
 end
