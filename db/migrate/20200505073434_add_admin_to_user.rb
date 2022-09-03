ba97a4fe84b6e0027ae7186fc938828728f8e0a1
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-03 16:08:11 +0800

    Delete db directory

--- db/migrate/20200505073434_add_admin_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddAdminToUser < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :authorization, :integer, default: 0
-  end
-end
--- db/migrate/20200520041753_add_double_index_to_subscription.rb
+++
@@ -1,5 +0,0 @@
-class AddDoubleIndexToSubscription < ActiveRecord::Migration[6.0]
-  def change
-		add_index :subscriptions, [:user_id, :chatroom_id], unique: true
-  end
-end
